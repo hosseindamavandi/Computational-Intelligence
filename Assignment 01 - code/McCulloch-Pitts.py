a2172e98359d130d0f07643b81df94236180e576
hoseindamavandi <h.damavandi2012@yahoo.com>
2021-09-20 22:27:58 +0430

    add codes

---
+++ Assignment 01 - code/McCulloch-Pitts.py
@@ -0,0 +1,19 @@
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+a4 = int(input())
+
+w1 = float(input())
+w2 = float(input())
+w3 = float(input())
+w4 = float(input())
+
+soma = float(input())
+
+sum = (a1 * w1) + (a2 * w2) + (a3 * w3) + (a4 * w4)
+
+if sum > soma :
+    print(1)
+
+else:
+    print(0)
